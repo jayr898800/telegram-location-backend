06971176a89faebc5592c1dbfad34b362523083d
Jayr Pelobello <jayr898800@gmail.com>
2025-08-12 18:33:06 +0800

    2 lines fixed at the bottom of script [2025-08-12 18:33:03]

--- server.js
+++ server.js
@@ -114,4 +114,4 @@ app.post("/send-to-telegram", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(\`Server running on port \${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
